c97c4cd1576f5fdfa45c9457651afda51712aa0d
Tássio Auad <auadtassio@gmail.com>
2016-03-08 00:31:38 -0300

    fix: Treating exception in Measurer
    Avoiding index out of bound and others exceptions in Measurer.

--- app/src/main/java/com/tassioauad/pedometro/model/Measurer.java
+++ app/src/main/java/com/tassioauad/pedometro/model/Measurer.java
@@ -10,21 +10,33 @@ import java.util.List;
 
 public class Measurer {
 
-    private HashMap<ActivityType, Long> distancePerActivityType;
+    private HashMap<ActivityType, Long> distancePerActivityType = new HashMap<>();
 
     public Measurer(List<ActivityLocation> activityLocationList) {
-        ActivityType currentActivityType = activityLocationList.get(0).getActivityType();
-        Location currentLocation = activityLocationList.get(0).getLocation().getLocation();
-        long distance = 0;
-        for(int i = 1; i < activityLocationList.size(); i++) {
-            ActivityLocation activityLocation = activityLocationList.get(i);
-            if(currentActivityType == activityLocation.getActivityType()) {
-                distance += currentLocation.distanceTo(activityLocation.getLocation().getLocation());
-            } else {
-                distancePerActivityType.put(currentActivityType, distancePerActivityType.get(currentActivityType) + distance);
-                distance = 0;
-                currentLocation = activityLocation.getLocation().getLocation();
-                currentActivityType = activityLocation.getActivityType();
+        distancePerActivityType.put(ActivityType.IN_VEHICLE, 0l);
+        distancePerActivityType.put(ActivityType.ON_BICYCLE, 0l);
+        distancePerActivityType.put(ActivityType.ON_FOOT, 0l);
+        distancePerActivityType.put(ActivityType.STILL, 0l);
+        distancePerActivityType.put(ActivityType.UNKNOWN, 0l);
+        distancePerActivityType.put(ActivityType.TILTING, 0l);
+        distancePerActivityType.put(ActivityType.DEFAULT, 0l);
+        distancePerActivityType.put(ActivityType.WALKING, 0l);
+        distancePerActivityType.put(ActivityType.RUNNING, 0l);
+
+        if(activityLocationList.size() > 0) {
+            ActivityType currentActivityType = activityLocationList.get(0).getActivityType();
+            Location currentLocation = activityLocationList.get(0).getLocation().getLocation();
+            long distance = 0;
+            for(int i = 1; i < activityLocationList.size(); i++) {
+                ActivityLocation activityLocation = activityLocationList.get(i);
+                if(currentActivityType == activityLocation.getActivityType()) {
+                    distance += currentLocation.distanceTo(activityLocation.getLocation().getLocation());
+                } else {
+                    distancePerActivityType.put(currentActivityType, distancePerActivityType.get(currentActivityType) + distance);
+                    distance = 0;
+                    currentLocation = activityLocation.getLocation().getLocation();
+                    currentActivityType = activityLocation.getActivityType();
+                }
             }
         }
     }
