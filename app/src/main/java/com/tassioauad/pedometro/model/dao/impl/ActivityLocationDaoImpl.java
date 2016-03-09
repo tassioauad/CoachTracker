37ab210c0424cd4a6e34f2e8b126aec046711453
Tássio Auad <auadtassio@gmail.com>
2016-03-08 22:20:52 -0300

    fix: Calendar.clear is not working as expected
    Using calendar.set(FIELD, 0) instead of clear(), because the method is not setting 0 into the FIELD.

--- app/src/main/java/com/tassioauad/pedometro/model/dao/impl/ActivityLocationDaoImpl.java
+++ app/src/main/java/com/tassioauad/pedometro/model/dao/impl/ActivityLocationDaoImpl.java
@@ -53,10 +53,10 @@ public class ActivityLocationDaoImpl extends Dao implements ActivityLocationDao 
 
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(currentDay);
-        calendar.clear(Calendar.HOUR_OF_DAY);
-        calendar.clear(Calendar.MINUTE);
-        calendar.clear(Calendar.SECOND);
-        calendar.clear(Calendar.MILLISECOND);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
 
         currentDay = calendar.getTime();
         calendar.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_YEAR) + 1);
