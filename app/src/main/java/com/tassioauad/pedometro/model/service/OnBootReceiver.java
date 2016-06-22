230f35bbf2fd9291fd799654ac4213a446f51d63
Tássio Auad <auadtassio@gmail.com>
2016-06-22 00:32:02 -0300

    feat: Receiver to OnBootCompleted Created a BroadCastReceiver that will be called when the device boot is completed.

---
+++ app/src/main/java/com/tassioauad/pedometro/model/service/OnBootReceiver.java
@@ -0,0 +1,15 @@
+package com.tassioauad.pedometro.model.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnBootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Tracker tracker = new TrackerImpl(context);
+        if(tracker.isTracking()) {
+            tracker.startTrackingService();
+        }
+    }
+}
